d54be04ea9d54710aa85055f9d6e21506cf225b6
dhiazrionaldo <dhiazrionaldo@gmail.com>
2024-09-22 09:40:37 +0700

    bugfixing

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,97 +1,32 @@
 import { NextPage } from 'next'
 import NextLink from 'next/link'
-import { Box, Center, Stack, Text } from '@chakra-ui/react'
-import { Auth, Link } from '@saas-ui/react'
-import { Features } from 'components/features'
-import { BackgroundGradient } from 'components/gradients/background-gradient'
-import { Section } from 'components/section'
-import siteConfig from 'data/config'
-
-import { FaGithub, FaGoogle } from 'react-icons/fa'
+import { Box, Center, Stack, Button, Text } from '@chakra-ui/react'
 import { PageTransition } from 'components/motion/page-transition'
-
-const providers = {
-  google: {
-    name: 'Google',
-    icon: FaGoogle,
-  },
-  github: {
-    name: 'Github',
-    icon: FaGithub,
-    variant: 'solid',
-  },
-}
 
 const Login: NextPage = () => {
   return (
-    <Section height="100vh" innerWidth="container.xl">
-      <BackgroundGradient
-        zIndex="-1"
-        width={{ base: 'full', lg: '50%' }}
-        left="auto"
-        right="0"
-        borderLeftWidth="1px"
-        borderColor="gray.200"
-        _dark={{
-          borderColor: 'gray.700',
-        }}
-      />
+    <Center height="100vh">
       <PageTransition height="100%" display="flex" alignItems="center">
         <Stack
-          width="100%"
-          alignItems={{ base: 'center', lg: 'flex-start' }}
-          spacing="20"
-          flexDirection={{ base: 'column', lg: 'row' }}
+          spacing="4"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
         >
-          <Box pe="20">
-            <NextLink href="/">
-              <Box
-                as={siteConfig.logo}
-                width="160px"
-                ms="4"
-                mb={{ base: 0, lg: 16 }}
-              />
-            </NextLink>
-            <Features
-              display={{ base: 'none', lg: 'flex' }}
-              columns={1}
-              iconSize={4}
-              flex="1"
-              py="0"
-              ps="0"
-              maxW={{ base: '100%', xl: '80%' }}
-              features={siteConfig.signup.features.map((feature) => ({
-                iconPosition: 'left',
-                variant: 'left-icon',
+          {/* 'Under Development' message */}
+          <Text fontSize="2xl" fontWeight="bold">
+            Under Development
+          </Text>
 
-                ...feature,
-              }))}
-            />
-          </Box>
-          <Center height="100%" flex="1">
-            <Box width="container.sm" pt="8" px="8">
-              <Auth
-                view="signup"
-                title={siteConfig.signup.title}
-                providers={providers}
-                loginLink={<Link href="/login">Log in</Link>}
-              >
-                <Text color="muted" fontSize="sm">
-                  By signing up you agree to our{' '}
-                  <Link href={siteConfig.termsUrl} color="white">
-                    Terms of Service
-                  </Link>{' '}
-                  and{' '}
-                  <Link href={siteConfig.privacyUrl} color="white">
-                    Privacy Policy
-                  </Link>
-                </Text>
-              </Auth>
-            </Box>
-          </Center>
+          {/* Back button to navigate to the home page */}
+          <NextLink href="/" passHref>
+            <Button as="a" colorScheme="blue">
+              Go Back to Home
+            </Button>
+          </NextLink>
         </Stack>
       </PageTransition>
-    </Section>
+    </Center>
   )
 }
 
